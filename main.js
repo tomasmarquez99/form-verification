3fb56fe502f579aa2c762ff9ea35288b39997ea7
tomasmarquez99 <btomasmarquez@gmail.com>
2021-10-22 18:30:33 -0300

    style and funcionality

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 let id = (id) => document.getElementById(id);
+
 let classes = (classes) => document.getElementsByClassName(classes);
 
 let username = id("username"),
-email = id("email"),
-password = id("password"),
-form = id("form"),
+  email = id("email"),
+  password = id("password"),
+  form = id("form"),
+  
+  errorMsg = classes("error"),
+  successIcon = classes("success-icon"),
+  failureIcon = classes("failure-icon");
 
-errorMsg = classes("error"),
-successIcon = classes("success-icon"),
-failureIcon = classes("failure-icon");
-
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    engine(username, 0, "Username cannot be blank");
-    engine(email, 1, "Email cannot be blank");
-    engine(password, 2, "Password cannot be blank");
-});
 
 
 let engine = (id, serial, message) => {
@@ -38,4 +31,11 @@ if (id.value.trim() === "") {
 
 }};
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    engine(username, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(password, 2, "Password cannot be blank");
+  });
 
